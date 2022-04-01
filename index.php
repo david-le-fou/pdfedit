1feb39c1ac461192f501c716d8e0b79cddb56f5c
david.propitech <david.propitech@gmail.com>
2022-04-01 14:36:38 +0300

    test 2 ok

--- index.php
+++ index.php
@@ -2,22 +2,26 @@
 use setasign\Fpdi\Fpdi;
 
 require_once('fpdf/fpdf.php');
+// require_once('html2pdf/src/Html2Pdf.php');
 require_once('fpdi2/src/autoload.php');
 
 // initiate FPDI
 $pdf = new Fpdi();
+
 // add a page
 $pdf->AddPage();
 // set the source file
-$pdf->setSourceFile('test.pdf');
+$pdf->setSourceFile('LPM1.pdf');
 // import page 1
 $tplIdx = $pdf->importPage(1);
 // use the imported page and place it at position 10,10 with a width of 100 mm
-$pdf->useTemplate($tplIdx, 10, 10, 100);
+$pdf->useTemplate($tplIdx, 5, 5, 200);
 // now write some text above the imported page
+$pdf->Image('QR.jpg',160,20,35);
+
 $pdf->SetFont('Helvetica');
 $pdf->SetTextColor(255, 0, 0);
-$pdf->SetXY(30, 30);
+$pdf->SetXY(100, 113);
 $pdf->Write(0, 'This is just a simple text vody');
 
 $pdf->Output('I', 'generated.pdf');
